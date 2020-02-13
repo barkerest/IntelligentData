6ec5d12a9c882b6dcaf3fef729a6b61102afcc43
Beau Barker <bbarker@ckindustrial.com>
2020-02-13 17:13:14 -0500

    Start adding queryable extensions.

---
+++ IntelligentData.Tests/QueryableExtensions_Should.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+using IntelligentData.Extensions;
+using IntelligentData.Tests.Examples;
+using Xunit;
+using Xunit.Abstractions;
+using Xunit.Sdk;
+
+namespace IntelligentData.Tests
+{
+    public class QueryableExtensions_Should
+    {
+        private ExampleContext    _db;
+        private ITestOutputHelper _output;
+
+        public QueryableExtensions_Should(ITestOutputHelper output)
+        {
+            _output = output ?? throw new ArgumentNullException(nameof(output));
+            _db     = ExampleContext.CreateContext(false);
+        }
+
+        public static IEnumerable<object[]> GetQueries()
+        {
+            return new Func<ExampleContext, IQueryable<ReadOnlyEntity>>[]
+                {
+                    // simple select all
+                    db => db.ReadOnlyEntities,
+                    
+                    // with a simple filter.
+                    db => db.ReadOnlyEntities.Where(r => r.Name != "John"),
+                    
+                    // with a subquery
+                    db => db.ReadOnlyEntities.Where(r => db.ReadInsertEntities.Select(ri => ri.ID).Contains(r.ID)),
+                    
+                    // with a value list.
+                    db => db.ReadOnlyEntities.Where(r => new []{1,3,5,7,9}.Contains(r.ID)),
+                    
+                    // with a value list parameter.
+                    db =>
+                    {
+                        var list = new[] {1, 3, 5, 7, 9};
+                        return db.ReadOnlyEntities.Where(r => list.Contains(r.ID));
+                    }
+                }
+                .Select(x => new object[] {x});
+        }
+        
+        [Theory]
+        [MemberData(nameof(GetQueries))]
+        public void GetSqlString(Func<ExampleContext,IQueryable<ReadOnlyEntity>> getQuery)
+        {
+            var query = getQuery(_db);
+            Assert.NotNull(query);
+            _output.WriteLine(query.ToString());
+
+            var task = Task.Run<string>(() => query.GetSqlString());
+
+            if (task.Wait(TimeSpan.FromSeconds(10)))
+            {
+                _output.WriteLine(task.Result);
+            }
+            else
+            {
+                throw new XunitException("Timeout waiting for SQL generation.");
+            }
+        }
+    }
+}
---
+++ IntelligentData/Extensions/QueryableExtensions.cs
@@ -0,0 +1,183 @@
+﻿using System;
+using System.Collections;
+using System.Diagnostics;
+using System.Linq;
+using System.Reflection;
+using Microsoft.EntityFrameworkCore.Query;
+using Microsoft.EntityFrameworkCore.Query.Internal;
+using Microsoft.EntityFrameworkCore.Query.SqlExpressions;
+using Microsoft.EntityFrameworkCore.Storage;
+
+namespace IntelligentData.Extensions
+{
+    /// <summary>
+    /// Extension methods for queryable interfaces.
+    /// </summary>
+    public static class QueryableExtensions
+    {
+        private static object PrivateField(this object obj, string fieldName)
+            => obj?.GetType().GetField(fieldName, BindingFlags.Instance | BindingFlags.NonPublic)?.GetValue(obj);
+
+        private static T PrivateField<T>(this object obj, string fieldName)
+            => (T) PrivateField(obj, fieldName);
+
+        private static void PrivateSet(this object obj, string propName, object value)
+        {
+            var type = obj.GetType();
+            var prop = type.GetProperty(propName, BindingFlags.Instance | BindingFlags.Public)
+                       ?? throw new InvalidOperationException($"Missing public {propName} property.");
+            var setter = prop.GetSetMethod(true)
+                         ?? throw new InvalidOperationException($"Missing non-public setter on {propName} property.");
+
+            setter.Invoke(obj, new object[]{value});
+        }
+
+        /// <summary>
+        /// Gets the SQL string from this query.
+        /// </summary>
+        /// <param name="query"></param>
+        /// <returns></returns>
+        /// <exception cref="InvalidOperationException"></exception>
+        public static string GetSqlString(this IQueryable query)
+        {
+            var result = TryGetSqlString(query, out var value);
+            if (!result) throw new InvalidOperationException(value);
+            return value;
+        }
+
+        private static void InPatch_Expression(SqlExpression expression)
+        {
+            switch (expression)
+            {
+                case SqlUnaryExpression sqlUnaryExpression:
+                    {
+                        if (sqlUnaryExpression.Operand is InExpression inex)
+                        {
+                                sqlUnaryExpression.PrivateSet("Operand", ProcessInExpression(inex));
+                        }
+                        else
+                        {
+                                InPatch_Expression(sqlUnaryExpression.Operand);
+                        }
+                    }
+                    break;
+                
+                case CaseExpression caseExpression:
+                    foreach (var c in caseExpression.WhenClauses)
+                    {
+                        if (c.Test is InExpression inex)
+                        {
+                            c.PrivateSet("Test", ProcessInExpression(inex));
+                        }
+                        else
+                        {
+                            InPatch_Expression(c.Test);
+                        }
+                    }
+
+                    break;
+
+                case ExistsExpression existsExpression:
+                case LikeExpression likeExpression:
+                case RowNumberExpression rowNumberExpression:
+                case ScalarSubqueryExpression scalarSubqueryExpression:
+                case SqlBinaryExpression sqlBinaryExpression:
+                case SqlFragmentExpression sqlFragmentExpression:
+                case SqlFunctionExpression sqlFunctionExpression:
+                    break;
+                
+                case ColumnExpression _:
+                case SqlConstantExpression _:
+                case SqlParameterExpression _:
+                    break;
+                default:
+                    throw new InvalidOperationException($"Unknown expression type: {expression.GetType()}");
+            }
+        }
+
+        private static InExpression ProcessInExpression(InExpression expression)
+        {
+            // TODO: Process!!
+            return expression;
+        }
+        
+        private static void InPatch_Having(SelectExpression expression)
+        {
+            if (expression.Having is null) return;
+
+            if (expression.Having is InExpression inex)
+            {
+                expression.PrivateSet("Having", ProcessInExpression(inex));
+            }
+            else
+            {
+                InPatch_Expression(expression.Having);
+            }
+        }
+
+        private static void InPatch_Predicate(SelectExpression expression)
+        {
+            if (expression.Predicate is null) return;
+
+            if (expression.Predicate is InExpression inex)
+            {
+                expression.PrivateSet("Predicate", ProcessInExpression(inex));
+            }
+            else
+            {
+                InPatch_Expression(expression.Predicate);
+            }
+        }
+        
+        /// <summary>
+        /// Tries to get the SQL string from this query.
+        /// </summary>
+        /// <param name="query"></param>
+        /// <param name="value"></param>
+        /// <returns></returns>
+        public static bool TryGetSqlString(this IQueryable query, out string value)
+        {
+            value = "";
+
+            var provider = query.Provider as EntityQueryProvider;
+            if (provider is null)
+            {
+                value = "Does not use an entity query provider.";
+                return false;
+            }
+
+            var enumerator   = provider.Execute<IEnumerable>(query.Expression).GetEnumerator();
+            
+            var commandCache = enumerator.PrivateField<RelationalCommandCache>("_relationalCommandCache");
+            
+            if (commandCache is null)
+            {
+                value = "Does not appear to be a relational command.";
+                return false;
+            }
+
+            var selectExpression = commandCache.PrivateField<SelectExpression>("_selectExpression");
+            if (selectExpression is null)
+            {
+                value = "Does not provide a select expression.";
+                return false;
+            }
+
+            InPatch_Having(selectExpression);
+            InPatch_Predicate(selectExpression);
+            
+            var factory = commandCache.PrivateField<IQuerySqlGeneratorFactory>("_querySqlGeneratorFactory");
+            if (factory is null)
+            {
+                value = "Does not provide a SQL generator factory.";
+                return false;
+            }
+
+            var generator = factory.Create();
+            var command = generator.GetCommand(selectExpression);
+            value = command.CommandText;
+            
+            return true;
+        }
+    }
+}
