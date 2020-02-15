42d389d4489fb8506bfaa13fd2a984b9572b97ca
Beau Barker <beau@barkerest.com>
2020-02-15 11:03:24 -0500

    Move common code to QueryInfo internal class.

--- IntelligentData/Extensions/QueryableExtensions.cs
+++ IntelligentData/Extensions/QueryableExtensions.cs
@@ -2,6 +2,7 @@
 using System.Collections;
 using System.Linq;
 using IntelligentData.Internal;
+using Microsoft.EntityFrameworkCore.Internal;
 using Microsoft.EntityFrameworkCore.Query;
 using Microsoft.EntityFrameworkCore.Query.Internal;
 using Microsoft.EntityFrameworkCore.Query.SqlExpressions;
@@ -36,49 +37,13 @@ namespace IntelligentData.Extensions
         {
             value = "";
 
-            var provider = query.Provider as EntityQueryProvider;
-            if (provider is null)
-            {
-                value = "Does not use an entity query provider.";
-                return false;
-            }
-
-            // ReSharper disable once EF1001
-            var enumerator = provider.Execute<IEnumerable>(query.Expression).GetEnumerator();
-
-            var commandCache = enumerator.GetNonPublicField<RelationalCommandCache>("_relationalCommandCache");
-
-            if (commandCache is null)
-            {
-                value = "Does not appear to be a relational command.";
-                return false;
-            }
-
-            var queryContext = enumerator.GetNonPublicField<RelationalQueryContext>("_relationalQueryContext");
-            if (queryContext is null)
-            {
-                value = "Does not provide a query context.";
-                return false;
-            }
+            var queryInfo = QueryInfo.Create(query, out value);
+            if (queryInfo is null) return false;
             
-            var selectExpression = commandCache.GetNonPublicField<SelectExpression>("_selectExpression");
-            if (selectExpression is null)
-            {
-                value = "Does not provide a select expression.";
-                return false;
-            }
-
-            selectExpression.PatchInExpressions(queryContext);
-
-            var factory = commandCache.GetNonPublicField<IQuerySqlGeneratorFactory>("_querySqlGeneratorFactory");
-            if (factory is null)
-            {
-                value = "Does not provide a SQL generator factory.";
-                return false;
-            }
-
-            var generator = factory.Create();
-            var command   = generator.GetCommand(selectExpression);
+            queryInfo.Expression.PatchInExpressions(queryInfo.Context);
+            
+            var generator = queryInfo.SqlGeneratorFactory.Create();
+            var command   = generator.GetCommand(queryInfo.Expression);
             value = command.CommandText;
 
             return true;
---
+++ IntelligentData/Internal/QueryInfo.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using IntelligentData.Extensions;
+using Microsoft.EntityFrameworkCore.Query;
+using Microsoft.EntityFrameworkCore.Query.Internal;
+using Microsoft.EntityFrameworkCore.Query.SqlExpressions;
+
+namespace IntelligentData.Internal
+{
+    internal class QueryInfo
+    {
+        private QueryInfo()
+        {
+            
+        }
+        
+        public SelectExpression Expression { get; private set; }
+        
+        public RelationalQueryContext Context { get; private set; }
+        
+        public RelationalCommandCache CommandCache { get; private set; }
+        
+        public IQuerySqlGeneratorFactory SqlGeneratorFactory { get; private set; }
+
+        private Dictionary<string, object> _params = new Dictionary<string, object>();
+        
+        public static QueryInfo Create(IQueryable query)
+            => Create(query, out _);
+
+        public static QueryInfo Create(IQueryable query, out string message)
+        {
+            message = "";
+            
+            if (query is null) throw new ArgumentNullException(nameof(query));
+            
+            var ret = new QueryInfo();
+            
+            var provider = query.Provider as EntityQueryProvider;
+            if (provider is null)
+            {
+                message = "Does not use an entity query provider.";
+                return null;
+            }
+
+            // ReSharper disable once EF1001
+            var enumerator = provider.Execute<IEnumerable>(query.Expression).GetEnumerator();
+
+            ret.CommandCache = enumerator.GetNonPublicField<RelationalCommandCache>("_relationalCommandCache");
+
+            if (ret.CommandCache is null)
+            {
+                message = "Does not appear to be a relational command.";
+                return null;
+            }
+
+            ret.Context = enumerator.GetNonPublicField<RelationalQueryContext>("_relationalQueryContext");
+            if (ret.Context is null)
+            {
+                message = "Does not provide a query context.";
+                return null;
+            }
+            
+            ret.Expression = ret.CommandCache.GetNonPublicField<SelectExpression>("_selectExpression");
+            if (ret.Expression is null)
+            {
+                message = "Does not provide a select expression.";
+                return null;
+            }
+
+            ret.SqlGeneratorFactory = ret.CommandCache.GetNonPublicField<IQuerySqlGeneratorFactory>("_querySqlGeneratorFactory");
+            if (ret.SqlGeneratorFactory is null)
+            {
+                message = "Does not provide a SQL generator factory.";
+                return null;
+            }
+            
+            return ret;
+        }
+        
+    }
+}
