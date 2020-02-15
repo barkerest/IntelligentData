543fd683ae2771f70e9964beb52bcb15d773dacc
Beau Barker <beau@barkerest.com>
2020-02-15 14:16:05 -0500

    Add SQL knowledge interface for future extensions.

---
+++ IntelligentData/Extensions/SqlKnowledgeExtensions.cs
@@ -0,0 +1,88 @@
+﻿using System.Collections.Generic;
+using System.Text;
+using IntelligentData.Interfaces;
+
+namespace IntelligentData.Extensions
+{
+    /// <summary>
+    /// Extensions to the SQL knowledge interface.
+    /// </summary>
+    public static class SqlKnowledgeExtensions
+    {
+        /// <summary>
+        /// Quotes an object name.
+        /// </summary>
+        /// <param name="knowledge"></param>
+        /// <param name="objectName"></param>
+        /// <returns></returns>
+        public static string QuoteObjectName(this ISqlKnowledge knowledge, string objectName)
+        {
+            if (knowledge is null) return null;
+            if (string.IsNullOrEmpty(objectName)) return "";
+            
+            StringBuilder ret;
+            if (knowledge.EscapeObjectName != null)
+            {
+                ret = new StringBuilder(knowledge.EscapeObjectName(objectName));
+            }
+            else
+            {
+                ret = new StringBuilder(objectName);
+                ret.Replace(
+                    knowledge.ObjectCloseQuote,
+                    knowledge.ObjectCloseQuote + knowledge.ObjectCloseQuote
+                );
+            }
+
+            ret.Insert(0, knowledge.ObjectOpenQuote);
+            ret.Append(knowledge.ObjectCloseQuote);
+            
+            return ret.ToString();
+        }
+
+        /// <summary>
+        /// Concatenates two values.
+        /// </summary>
+        /// <param name="knowledge"></param>
+        /// <param name="value1"></param>
+        /// <param name="value2"></param>
+        /// <returns></returns>
+        public static string ConcatValues(this ISqlKnowledge knowledge, string value1, string value2)
+        {
+            var ret = new StringBuilder();
+
+            if (string.IsNullOrEmpty(knowledge.ConcatStringBefore))
+            {
+                ret.Append('(');
+            }
+            else
+            {
+                ret.Append(knowledge.ConcatStringBefore);
+            }
+
+            ret.Append(value1);
+            if (knowledge.ConcatStringMid == ",")
+            {
+                ret.Append(", ");
+            }
+            else
+            {
+                ret.Append(' ').Append(knowledge.ConcatStringMid).Append(' ');
+            }
+
+            ret.Append(value2);
+            if (string.IsNullOrEmpty(knowledge.ConcatStringBefore))
+            {
+                ret.Append(')');
+            }
+            else
+            {
+                ret.Append(knowledge.ConcatStringAfter);
+            }
+            
+            return ret.ToString();
+        }
+
+        
+    }
+}
---
+++ IntelligentData/Interfaces/ISqlKnowledge.cs
@@ -0,0 +1,81 @@
+﻿using System;
+
+namespace IntelligentData.Interfaces
+{
+    /// <summary>
+    /// An interface that supplies knowledge about the SQL language.
+    /// </summary>
+    public interface ISqlKnowledge
+    {
+        /// <summary>
+        /// The name of the engine this knowledge applies to.
+        /// </summary>
+        string EngineName { get; }
+        
+        /// <summary>
+        /// The character used to start quoting an object name.
+        /// </summary>
+        string ObjectOpenQuote { get; }
+        
+        /// <summary>
+        /// The character used to stop quoting an object name.
+        /// </summary>
+        string ObjectCloseQuote { get; }
+        
+        /// <summary>
+        /// Optional function used to escape object names.
+        /// </summary>
+        /// <remarks>
+        /// The default behavior is to double up closing quotes within the object name.
+        /// This function should not wrap the object name in quotes.
+        /// </remarks>
+        Func<string,string> EscapeObjectName { get; }
+
+        /// <summary>
+        /// Code to insert before a string concatenation.
+        /// </summary>
+        /// <remarks>
+        /// For languages that use a function, this would likely be the function name and opening parenthesis (eg - "CONCAT(").
+        /// For languages that use an operator, this would be blank.
+        /// </remarks>
+        string ConcatStringBefore { get; }
+        
+        /// <summary>
+        /// Code to insert between values in a string concatenation.
+        /// </summary>
+        /// <remarks>
+        /// For languages that use a function, this would likely be a comma.
+        /// For languages that use an operator, this would be the operator.
+        /// </remarks>
+        string ConcatStringMid { get; }
+        
+        /// <summary>
+        /// Code to insert after a string concatenation.
+        /// </summary>
+        /// <remarks>
+        /// For languages that use a function, this would likely be the closing parenthesis.
+        /// For languages that use an operator, this would be blank.
+        /// </remarks>
+        string ConcatStringAfter { get; }
+        
+        /// <summary>
+        /// The command text used to retrieve the ID from the last insert operation.
+        /// </summary>
+        string GetLastInsertedIdCommand { get; }
+        
+        /// <summary>
+        /// Does the language support table aliases in DELETE statements.
+        /// </summary>
+        bool DeleteSupportsTableAliases { get; }
+        
+        /// <summary>
+        /// Does the language support table aliases in UPDATE statements.
+        /// </summary>
+        bool UpdateSupportsTableAliases { get; }
+        
+        /// <summary>
+        /// Does the language support FROM clauses in UPDATE statements.
+        /// </summary>
+        bool UpdateSupportsFromClause { get; }
+    }
+}
